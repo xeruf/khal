b0577ebbb37df3eac1e2ede3e43a28072c3407e5
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2021-05-08 20:57:00 +0900

    update method docs

--- khal/utils.py
+++ khal/utils.py
@@ -189,6 +189,7 @@ def get_wrapped_text(widget):
 
 
 def human_formatter(format_string, width=None, colors=True):
+    """Create a formatter that formats events to be human readable."""
     def fmt(rows):
         single = type(rows) == dict
         if single:
@@ -210,6 +211,7 @@ def human_formatter(format_string, width=None, colors=True):
 
 
 def json_formatter(fields):
+    """Create a formatter that formats events in JSON."""
     def fmt(rows):
         single = type(rows) == dict
         if single:
